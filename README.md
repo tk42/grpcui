4b9f643e0eeab22232e96b17b75254f3fd5dfc51
tk42 <nsplat@gmail.com>
2022-05-14 18:13:46 +0900

    Added wait-for-it.sh

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Docker image for fullstorydev/grpcui
 
 ## Usage
+Try to connect to the server if possible after 5 sec.
 ```
-docker run -eGRPCUI_SERVER=localhost:8888 -p8080:8080 ghcr.io/tk42/grpcui
+docker run -eGRPCUI_SERVER=localhost:8888 -GRPCUI_WAIT=5 -p8080:8080 ghcr.io/tk42/grpcui
 ```
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ if [[ -f /protoset/service.protoset ]]; then
     PROTOSET="-protoset /protoset/service.protoset"
 fi
 
-grpcui -bind 0.0.0.0 ${PROTOSET} -plaintext -port 8080 ${GRPCUI_SERVER:-}
+./wait-for-it.sh ${GRPCUI_SERVER:-} --timeout=${GRPCUI_WAIT} --strict -- grpcui -bind 0.0.0.0 ${PROTOSET} -plaintext -port 8080 ${GRPCUI_SERVER:-}
