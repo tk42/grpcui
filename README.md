1a4c642b9a0f8fed3161f9d14dcbe7ce511d9556
tk42 <nsplat@gmail.com>
2022-05-14 18:39:42 +0900

    Fixed TIMEOUT

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Docker image for fullstorydev/grpcui
 
 ## Usage
-Try to connect to the server if possible after 5 sec.
+Try to connect to the server if possible after 5 secs. (1 secs if it's not specified.)
 ```
 docker run -eGRPCUI_SERVER=localhost:8888 -GRPCUI_WAIT=5 -p8080:8080 ghcr.io/tk42/grpcui
 ```
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,7 @@ if [[ -f /protoset/service.protoset ]]; then
     PROTOSET="-protoset /protoset/service.protoset"
 fi
 
-./wait-for-it.sh ${GRPCUI_SERVER:-} --timeout=${GRPCUI_WAIT} --strict -- grpcui -bind 0.0.0.0 ${PROTOSET} -plaintext -port 8080 ${GRPCUI_SERVER:-}
+BASEDIR=$(dirname "$0")
+TIMEOUT=${GRPCUI_WAIT:-1} # Freeze if set 0
+
+${BASEDIR}/wait-for-it.sh ${GRPCUI_SERVER:-} --timeout=${TIMEOUT} --strict -- grpcui -bind 0.0.0.0 ${PROTOSET} -plaintext -port 8080 ${GRPCUI_SERVER:-}
